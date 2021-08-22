80b6a4a83eb99983c6d9e9528e231ec593e2d4fa
Vladimir Siminenko <vsim1964@gmail.com>
2021-08-22 19:17:42 +0300

    Фавиконки и пиксель перфект

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,7 @@ const copy = (done) => {
       "source/*.ico",
       "source/img/**/*.svg",
       "!source/img/icons/*.svg",
+      "source/*.webmanifest",
     ], {
       base: "source"
     })
@@ -146,7 +147,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.sass", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
